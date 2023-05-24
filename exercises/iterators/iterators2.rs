7d088b851ee2e04c2b22bc17ddacc6a0869508f0
Cameron Lane <crlane@adamanteus.com>
2023-05-24 13:25:59 +0000

    through iterators2

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,16 +3,21 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
-    let mut c = input.chars();
+    let mut c = input.trim().chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            println!("{}", first);
+            let C = first.to_uppercase().to_string();
+            c.fold(C, |mut acc, n| {
+                acc.push(n);
+                acc
+            })
+        }
     }
 }
 
@@ -21,7 +26,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 // Step 3.
@@ -29,7 +34,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    capitalize_words_vector(words)
+        .into_iter() // TODO: why does this only work with into_iter?
+        .filter(|w| !w.is_empty())
+        .collect::<Vec<_>>()
+        .join(" ")
 }
 
 #[cfg(test)]
